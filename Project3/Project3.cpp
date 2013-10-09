82ec6bddf0c8ce57a344874b13015f69c02d8a8e
OPSand <o.p.sand@live.com>
2013-10-09 11:12:57 +0200

    Armadillo + lib setup

---
+++ Project3/Project3.cpp
@@ -0,0 +1,31 @@
+// Project3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include"armadillo"
+#include<list>
+
+using namespace arma;
+using namespace std; 
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	mat A = randu<mat>(5,5);
+	cout << det(A) << endl << endl;
+
+	mat L, U, P;
+	if( lu(L, U, P, A) )
+	{
+		cout << "LU success!" << endl;
+		cout << L << endl << endl;
+		cout << U << endl << endl;
+	}
+
+	vec v = randu<vec>(5);
+	vec x = solve(L, v);
+	cout << x << endl << endl;
+
+	getchar(); // pause
+
+	return 0;
+}
